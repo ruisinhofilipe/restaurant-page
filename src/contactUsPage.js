7bdf3b582c604a637e386bd2f0f7e7991115b478
Rui Silva <ruisinhofilipe@gmail.com>
2023-02-01 14:11:47 +0100

    Added content to the contact us page

--- src/contactUsPage.js
+++ src/contactUsPage.js
@@ -31,8 +31,47 @@ const contactUsPage = () => {
     // Right side of contactUsContainer
     const infoContainer = elementFactory('div', { class: 'infoContainer right' }).build();
     //// righ side content
-    const openingHoursContainer = elementFactory('div', { class: 'OpeningHoursContainer' }, 'Opening-hours').build();
+    const openingHoursContainer = elementFactory('lu', { class: 'OpeningHoursContainer' }, 'Opening-hours').build();
+    for (let i = 1; i <= 7; i++) {
+        const day = elementFactory('li', { class: 'openingDays' }).build();
+        switch (i) {
+            case 1:
+                day.textContent = 'Monday: closed.';
+                break;
+            case 2:
+                day.textContent = 'Tuesday: 6pm - 23pm';
+                break;
+            case 3:
+                day.textContent = 'Wednesday: 11:30am - 4pm and 6pm - 23pm';
+                break;
+            case 4:
+                day.textContent = 'Thursday: 11:30am - 4pm and 6pm - 23pm';
+                break;
+            case 5:
+                day.textContent = 'Friday: 11:30am - 4pm and 6pm - 1am';
+                break;
+            case 6:
+                day.textContent = 'Saturday: 11:30am - 4pm and 6pm - 1am';
+                break;
+            case 7:
+                day.textContent = 'Sunday: 4pm - 22pm';
+                break;
+            default:
+                day.textContent = 'Something went wrong';
+                break;
+        }
+        openingHoursContainer.appendChild(day);
+    }
     const contactContainer = elementFactory('div', { class: 'contactContainer' }, 'Contact info').build();
+    for (let i = 1; i <= 2; i++) {
+        const info = elementFactory('div', { class: 'info' }).build();
+        if (i === 1) {
+            info.textContent = 'Email address: fakeRestaurantEmail@fake.fake';
+        } else {
+            info.textContent = 'Phone number = +0 000 000 000';
+        }
+        contactContainer.appendChild(info);
+    }
     infoContainer.appendChild(openingHoursContainer);
     infoContainer.appendChild(contactContainer);
 
